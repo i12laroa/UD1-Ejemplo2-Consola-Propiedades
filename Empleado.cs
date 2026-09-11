03631270a7e29537bd8ee66b1060dddb7ceb9088
i12laroa <albertolamarca@gmail.com>
2026-09-11 17:46:18 +0200

    Agregar archivos de proyecto.

---
+++ Empleado.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UD1_Ejemplo2_Consola_Propiedades
+{
+    public class Empleado
+    {
+
+        private double salario;
+        public int CodigoEmpleado { get; set; }
+        public string Nombre { get; set; }
+        public double Salario
+        {
+            get { return salario; }
+            set
+            {
+
+                if (value < 0)
+                {
+                    Console.WriteLine("No puedes cambiar el salario por un valor negativo");
+                }
+                else
+                {
+                    salario = value;
+                }
+            }   
+        }
+        public Empleado(int codigoEmpleado, string nombre, double salario)
+        {
+            CodigoEmpleado = codigoEmpleado;
+            Nombre = nombre;
+            Salario = salario;
+        }
+    }   
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UD1_Ejemplo2_Consola_Propiedades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Empleado empleado1 = new Empleado(1, "Alberto", 2000);
+            Console.WriteLine("Comienzo del curso");
+
+            Console.WriteLine("El primer trabajor se llama: " + empleado1.Nombre + " y su salario es: " + empleado1.Salario);
+            Console.WriteLine($"el primer trabajador se llama {empleado1.Nombre}");
+
+
+            empleado1.CodigoEmpleado = 20;
+        }
+    }
+}
