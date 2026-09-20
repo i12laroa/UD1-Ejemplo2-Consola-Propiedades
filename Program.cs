9ebdb741aabf0ecae652025d335fa37bc71721d4
i12laroa <albertolamarca@gmail.com>
2026-09-20 09:54:21 +0200

    eliminando variable

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,6 +19,8 @@ namespace UD1_Ejemplo2_Consola_Propiedades
 
 
             empleado1.CodigoEmpleado = 20;
+
+            
         }
     }
 }
